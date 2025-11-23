91542a0713862c61e824133bc9e1213ce1eb1304
nhuquynh2k6 <nhuquynhh2k6@gmail.com>
2025-11-23 20:56:36 +0700

    OOP buoi 5

---
+++ OOP_THB5/OOP_THB5/PhanSo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTHBuoi4
+{
+    internal class PhanSo
+    {
+        public PhanSo() : this(0, 1)
+        {
+        }
+
+        public PhanSo(int ts, int ms)
+        {
+            MS = ms;
+            TS = ts;
+            RutGon();
+        }
+
+        public void RutGon()
+        {
+            int ucln = GCD(Math.Abs(TS), Math.Abs(MS));
+            MS = MS / ucln;
+            TS = TS / ucln;
+        }
+
+        private int tS, mS;
+        public int TS { get => tS; set => tS = value; }
+
+        public int MS
+        {
+            get => mS; set
+            {
+                if (value == 0) return;
+                mS = value;
+            }
+        }
+
+        private int GCD(int a, int b)
+        {
+            return b == 0 ? a : GCD(a, b);
+        }
+
+        public static PhanSo operator +(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(a.TS + b.TS, a.MS + b.MS);
+        }
+
+        public static PhanSo operator -(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(a.TS - b.TS, a.MS - b.MS);
+        }
+
+        public static PhanSo operator *(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(a.TS * b.TS, a.MS * b.MS);
+        }
+
+        public static PhanSo operator /(PhanSo a, PhanSo b)
+        {
+            return a * new PhanSo(b.MS, b.TS);
+        }
+
+        public static bool operator ==(PhanSo a, PhanSo b)
+        {
+            return (a.TS == b.TS) && (a.MS == b.MS);
+        }
+
+        public static bool operator !=(PhanSo a, PhanSo b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator >(PhanSo a, PhanSo b)
+        {
+            return a.TS * b.MS > b.TS * a.MS;
+        }
+
+        public static bool operator <(PhanSo a, PhanSo b)
+        {
+            return !(a == b || a > b);
+        }
+
+        public static bool operator >=(PhanSo a, PhanSo b)
+        {
+            return !(a < b);
+        }
+
+        public static bool operator <=(PhanSo a, PhanSo b)
+        {
+            return !(a > b);
+        }
+
+        public static explicit operator double(PhanSo b)
+        {
+            return (double)b.TS / b.MS;
+        }
+    }
+}
---
+++ ZeroException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ZeroException:Exception
+    {
+        public ZeroException() : base("Input value must be not zero") 
+        { }
+    }
+
+    internal class OverValueException : Exception
+    {
+        public OverValueException() : base("Input value must be in 0..10")
+        { }
+    }
+
+}
